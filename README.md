3855bb1ab06beea856a7a87e825cbecb23043379
Too-Naive <sometimes.naive@hotmail.com>
2018-06-16 03:01:20 +0800

    Add daemon mode

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # passive-DDNS
-Design for hosker, It use in has public IP home network to auto change A record which domain use hosker ns.
+
+Design for [hosker](https://zhujike.com), It use in has public IP home network to auto change A record which domain use hosker ns.
 
 ## Usage
 
@@ -13,5 +14,14 @@ Copy `config.ini.default` to `config.ini`, parse your `email`, `token`, `header`
 python main.py
 ```
 
-## LICENSE
-AGPL v3
+## License
+
+[![](https://www.gnu.org/graphics/agplv3-155x51.png)](https://www.gnu.org/licenses/agpl-3.0.txt)
+
+Copyright (C) 2018 Too-Naive
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
--- main.py
+++ main.py
@@ -4,11 +4,26 @@
 #
 # This module is part of passive-DDNS and is released under
 # the AGPL v3 License: https://www.gnu.org/licenses/agpl-3.0.txt
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 import sys
 import time
 import urllib2
 import hostkerapi
+import os, signal
 from libpy import Log
+from subprocess import Popen
 from libpy.Config import Config
 
 external_ip_uri = 'https://www.appveyor.com/tools/my-ip.aspx'
@@ -36,6 +51,30 @@ def main():
 			raw, ipaddr = hostkerapi.get_record_ip()
 		time.sleep(600)
 
+def helpmsg():
+	print('Please using `[--daemon, -d] <file name>\' to run this program.\n\tusing `-kill` to kill daemon process (if running)')
+
 if __name__ == '__main__':
-	init()
-	main()
+	#if len(sys.argv) == 3:
+	#	if sys.argv[1] == '-d' or sys.argv[1] == '--daemon':
+	#		Popen(['python', sys.argv[0], '--daemon-start', sys.argv[2]])
+	#	elif sys.argv[1] == '--daemon-start':
+	#		with open(sys.argv[2], 'w') as fout:
+	#			fout.write(str(os.getpid()))
+	#	else:
+	#		helpmsg()
+	if len(sys.argv) == 2:
+		if sys.argv[1] == '-d' or sys.argv[1] == '--daemon':
+			Popen(['python', sys.argv[0], '--daemon-start'])
+		elif sys.argv[1] == '--daemon-start':
+			with open('pid', 'w') as fout:
+				fout.write(str(os.getpid()))
+			init()
+			main()
+		elif sys.argv[1] == '-kill':
+			with open('pid') as fin:
+				os.kill(int(fin.read()), signal.SIGINT)
+		else:
+			helpmsg()
+	else:
+		helpmsg()
